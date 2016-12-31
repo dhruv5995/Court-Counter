3ef29aa3da6f130e50db8cfd0d27ded0f148984a
Dhruv Shah <dhruv5995@gmail.com>
2016-12-31 16:35:24 +0530

    Created layout for the Court Counter app

---
+++ app/src/main/java/com/example/dhruv/courtcounter/MainActivity.java
@@ -0,0 +1,24 @@
+package com.example.dhruv.courtcounter;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+/**
+ * This app is a basket ball score tracker
+ */
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+
+    /**
+     * Display method for Team A
+     */
+    public void displayForTeamA(int score) {
+
+    }
+}
